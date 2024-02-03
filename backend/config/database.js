cd37a6510f26337543d7c2eb2d358e925bc9927e
Kyle <33107816+KG00D@users.noreply.github.com>
2024-02-03 13:06:33 -0500

    Fixing files

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,27 +1,33 @@
-// backend/config/database.js
 const config = require('./index');
 
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+const schema = db.schema;
+
 module.exports = {
-  development: {
-    storage: config.dbFile,
-    dialect: "sqlite",
-    seederStorage: "sequelize",
-    logQueryParameters: true,
-    typeValidation: true
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: 'sqlite',
+        seederStorage: 'sequelize'
     },
-    define: {
-      schema: process.env.SCHEMA
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        define: {
+            schema
+        }
     }
-  }
 };
- 
